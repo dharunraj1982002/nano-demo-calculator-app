21e5c51c5186575a3a790bd5d6db78cf2266d351
dharunraj1982002 <75053957+dharunraj1982002@users.noreply.github.com>
2023-08-23 12:20:19 +0000

    commit3

--- server.py
+++ server.py
@@ -9,17 +9,11 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    a=int(input())
-    b=int(input())
-    c=a+b
-    return c
+    return ''
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    a=int(input())
-    b=int(input())
-    c=a-b
-    return c
+    return ''
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
