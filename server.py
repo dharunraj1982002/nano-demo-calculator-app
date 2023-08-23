34c37db92916550c17d2c1d5a7878b23c7f927f3
dharunraj1982002 <dharunraj1982002@gmail.com>
2023-08-23 18:22:44 +0530

    commit7

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
@@ -9,11 +10,23 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return 'add'
+    try:
+        data = request.get_json()
+        numbers = numbers(data['first'], data['second'])
+        result = numbers.first + numbers.second
+        return jsonify({"result": result})
+    except KeyError:
+        return jsonify({"error": "Invalid request data"}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    try:
+        data = request.get_json()
+        numbers = numbers(data['first'], data['second'])
+        result = numbers.first - numbers.second
+        return jsonify({"result": result})
+    except KeyError:
+        return jsonify({"error": "Invalid request data"}), 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
